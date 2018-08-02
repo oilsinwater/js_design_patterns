6c9f3789462dede4f02e772410565429ec4921cf
Philliph Drummond <oilsinwater@gmail.com>
2018-08-02 01:12:40 -0700

    feat: Initializes repo

---
+++ constructor.js
@@ -0,0 +1,26 @@
+// traditional function-based syntax
+function Hero(name, specialAbility) {
+    // setting propery values
+    this.name = name;
+    this.specialAbility = specialAbility;
+
+    // declaring a method on the object
+    this.getDetails = function() {
+        return this.name + ' can ' + this.specialAbility;
+    };
+}
+
+// ES6 Class syntax
+class Hero {
+    constructor(name, specialAbility) {
+        // setting property values
+        this._name = name;
+        this._specialAbility = specialAbility;
+
+        //declaring a method on the object
+        this.getDetails = function() {
+            return `${this._name} can ${this._specialAbility}`;
+        };
+    }
+}
+
